554c25a444691e864a23a6870ebcae2680d0bb97
Philipp Arndt <2f.mail@gmx.de>
2018-12-21 10:55:08 +0100

    updated folder setup

--- src/main.js
+++ src/main.js
@@ -13,11 +13,18 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '/react/index.html'),
     protocol: 'file:',
     slashes: true
   }))
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: './renderer.js',
+  entry: './src/react/index.js',
   output: {
-    filename: './renderer-bundle.js'
+    filename: './src/react/index-bundle.js'
   },
   module: {
     loaders: [
